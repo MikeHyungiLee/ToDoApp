325835bf2377168edf034279ec1a55aa5548e93d
hyungilee <mike.hyungi.lee@gmail.com>
2020-02-13 18:57:50 +0900

    [add/update] ソースコードのコメントを作成・Singletonでアプリ作成(ToDoApplicationクラス)

--- app/src/main/java/com/hyungilee/todoapp/model/ToDoListItem.kt
+++ app/src/main/java/com/hyungilee/todoapp/model/ToDoListItem.kt
@@ -1,9 +1,20 @@
+/**
+ * @file ToDoListItem.kt
+ * @brief To-Doリストのタスク項目のData class
+ * @author Lee Hyungi
+ * @date 2020.02.12
+ */
 package com.hyungilee.todoapp.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * @brief リストのタスク情報のEntityクラス (データクラス)
+ * @par 概要
+ * リストのタスク項目情報デーブルの各項目を初期化する
+ */
 @Entity(tableName = "listItem_table")
 data class ToDoListItem(
     @PrimaryKey(autoGenerate = true)
--- app/src/main/java/com/hyungilee/todoapp/services/DataService.kt
+++ app/src/main/java/com/hyungilee/todoapp/services/DataService.kt
@@ -1,16 +1,20 @@
+/**
+ * @file DataService.kt
+ * @brief To-Doリストに出力するSampleデータを含むObjectクラス
+ * @author Lee Hyungi
+ * @date 2020.02.12
+ */
 package com.hyungilee.todoapp.services
 
 import com.hyungilee.todoapp.model.ToDoListItem
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 object DataService {
 
     // 現在の日付情報を取得
-    val dateFormat = SimpleDateFormat("dd/M/yyyy")
-    val currentDate = dateFormat.format(Date())
+    private val dateFormat = SimpleDateFormat("yyyy/mm/dd")
+    private val currentDate = dateFormat.format(Date())
 
     // Dummy data(To Do List)
     val listItems = listOf(
